21cacf906e7730b09f754e041529b7ccf16cd034
VAIO\Alejandro <alejocr7sr@gmail.com>
2021-03-17 14:33:19 -0500

    Pushing Project

--- Poke3D/ViewController.swift
+++ Poke3D/ViewController.swift
@@ -22,19 +22,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+
+        sceneView.automaticallyUpdatesLighting = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
+
+        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main) {
+            configuration.trackingImages = imageToTrack
+            configuration.maximumNumberOfTrackedImages = 5
+            print("Images succesfully added")
+        }
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -49,27 +52,48 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+
+            var imageRoute = ""
+            var imageName = ""
+
+            switch imageAnchor.referenceImage.name {
+            case "eevee-card":
+                imageRoute = "Eevee/Eevee.DAE"
+                imageName = "Eevee"
+            case "oddish-card":
+                imageRoute = "Oddish/Oddish.DAE"
+                imageName = "Oddish"
+            case "dragonite-card":
+                imageRoute = "Dragonite/Dragonite.dae"
+                imageName = "Dragonite"
+            case "houndoom-card":
+                imageRoute = "Houndoom/Houndoom.DAE"
+                imageName = "Houndoom"
+            default:
+                imageRoute = "Blastoise/Blastoise.DAE"
+                imageName = "Blastoise"
+            }
+
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+            
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi/2
+            
+            node.addChildNode(planeNode)
+            
+            if let pokeScene = SCNScene(named: "art.scnassets/\(imageRoute)") {
+                if let pokeNode = pokeScene.rootNode.childNode(withName: imageName, recursively: true) {
+                    pokeNode.eulerAngles.x = .pi / 2
+                    planeNode.addChildNode(pokeNode)
+                }
+            }
+        }
+
         return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
